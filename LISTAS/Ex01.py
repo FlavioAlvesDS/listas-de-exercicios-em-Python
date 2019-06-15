117d6a377bf2fc285f9d487db28ea2bf0280ec4d
flavinhofpa <50468932+flavinhofpa@users.noreply.github.com>
2019-06-15 20:08:33 -0300

    O arquivo foi excluido

--- LISTAS/Ex01.py
+++
@@ -1,9 +0,0 @@
-'''
-1) - Desenvolva um programa em python que receba 10 valores informados pelo usuário, armazene-os em um vetor, depois exiba os dados do vetor.
-'''
-
-numero = []
-
-for c in range(1,11,1):
-    numero.append(int(input(f"Informe o {c}º valor :")))
-print(numero)
